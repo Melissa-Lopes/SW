ee384540f47e8ea91f5c8816f1fbc2b0a299c69c
Melissa Lopes <80138971+Melissa-Lopes@users.noreply.github.com>
2022-06-03 11:21:40 -0300

    Add files via upload

---
+++ 27_05_2022/exemplo1.php
@@ -0,0 +1,23 @@
+<?php
+echo"1- Função sem retorno e sem parametros <br>";
+function teste(){
+    echo"Função 1";
+}
+teste();
+echo "<hr>";
+
+echo"2- Função com retorno e sem parametros <br>";
+function teste2(){
+    
+}
+
+
+echo"3- Função sem retorno e com parametros <br>";
+function teste3($a){
+    echo"$a <br>;
+}
+teste3 ('Oi galera');
+
+
+
+?>
---
+++ 27_05_2022/exemplo2.php
@@ -0,0 +1,16 @@
+<?php
+$numeros = [3,5,8,10,20,23,57];
+// criar uma função para somar os valores
+// de um vetor
+
+function soma($vetor){
+    $soma = 0;
+    for ($i=0; $i < count($vetor); $i++) { 
+  //echo "$vetor[$I] <br>";
+    }
+}
+soma($numeros);
+
+if($i%2=$a)
+echo"A soma é: $soma";
+?>
